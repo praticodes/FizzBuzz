f5a20c741bb41e0beefc4ae106673be194b393d4
Pratibha <108570654+praticodes@users.noreply.github.com>
2023-09-18 00:39:43 +0200

    debug

--- src/Multiples.java
+++ src/Multiples.java
@@ -2,7 +2,7 @@ public class Multiples {
     public static Object main(int j, int a, int b) {
         int count = 0;
         // Count multiples of 3 or 5 below 1000
-        for (int i = 0; i < j; i++)
+        for (int i = 1; i < j; i++)
 
             // Print our appropriate result.
             if (i % a == 0 || i % b == 0) {
